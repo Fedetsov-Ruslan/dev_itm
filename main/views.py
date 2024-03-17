3ee423579be46071bac75014f82b1dc86e858e22
Fedetsov-Ruslan <feduslan@gmail.com>
2024-03-17 23:00:58 +0300

    second_commit

--- main/views.py
+++ main/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 
 def index(request):
     context: dict ={
-        'title' : 'Home',
-        'content': 'Главная страница магазина - HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': True
+        'title' : 'HOME - Главная',
+        'content': 'Магазин мебели HOME'
     }
     return render(request, 'main/index.html',context)
 
 def about(request):
-    return HttpResponse("About page")
+    context: dict ={
+        'title' : 'О нас',
+        'content': 'О нас',
+        'text_on_page': 'текст описания нашего магазина'
+    }
+    return render(request, 'main/about.html',context)
 
