6acfaad29812faaa6f8199789fe69e6181e1a86f
Fedetsov-Ruslan <feduslan@gmail.com>
2024-03-18 10:21:30 +0300

    append super_admin

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,13 @@
+from enum import unique
+from tabnanny import verbose
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
