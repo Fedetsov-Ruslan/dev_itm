4e37495466af55030f3523d9ccd8f6414b9fb17f
Fedetsov-Ruslan <feduslan@gmail.com>
2024-03-19 10:39:49 +0300

    after  adding fixture for goods

--- goods/models.py
+++ goods/models.py
@@ -1,5 +1,7 @@
+from email.policy import default
 from enum import unique
 from tabnanny import verbose
+from unicodedata import category
 from django.db import models
 from django.forms import CharField
 
@@ -11,3 +13,26 @@ class Categories(models.Model):
         db_table = 'category'
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в процентах')
+    quantite = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantite}'
+    
