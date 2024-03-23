428a6fcf59498dc85576b0a4dc12c5dab23772e2
Fedetsov-Ruslan <feduslan@gmail.com>
2024-03-24 00:52:25 +0300

    Full text searh

--- goods/utils.py
+++ goods/utils.py
@@ -1,7 +1,8 @@
 from os import name
 from re import search
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
+from django.utils.termcolors import background
 from goods.models import Products
 
 
@@ -11,8 +12,11 @@ def q_serch(query):
     
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
-    
+
+    result = Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+    result = result.annotate(headline=SearchHeadline("name", query, start_sel='<span style="background-color: yellow;">', stop_sel="</span>",))
+    result = result.annotate(bodyline=SearchHeadline("description", query, start_sel='<span style="background-color: yellow;">', stop_sel="</span>",))
+    return result   
     #keywords = [word for word in query.split() if len(word)>2]
 #
     #q_objects = Q()
