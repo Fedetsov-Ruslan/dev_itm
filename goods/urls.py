6312227f4be1055efe75537147f93f4a370952b5
Fedetsov-Ruslan <feduslan@gmail.com>
2024-03-23 17:07:15 +0300

    Simple search created

--- goods/urls.py
+++ goods/urls.py
@@ -20,7 +20,10 @@ from goods import views
 app_name = 'goods'
 
 urlpatterns = [
-    path('<slug:category_slug>/',views.catalog, name ='index'),
+    path('search/',views.catalog, name ='search'),
+    path('<slug:category_slug>/',views.catalog, name ='index'),    
     path('product/<slug:product_slug>/',views.product, name ='product'),
+    
+
    
 ]
