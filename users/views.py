a91e84a413e3bb74bac2f64ea702ee2aebcfaf49
Fedetsov-Ruslan <feduslan@gmail.com>
2024-03-25 19:55:43 +0300

    login required decorator - redirect to target page

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.template import context
@@ -17,6 +17,10 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username} Вы вошли в аккаунт")
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -35,6 +39,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f"{user.username} Вы зарегистрировались и вошли аккаунт")
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
@@ -51,6 +56,7 @@ def profile(request):
         form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, "Профиль успешно обновлен")
             return HttpResponseRedirect(reverse('user:profile'))
     else:
         form = ProfileForm(instance=request.user)
@@ -62,5 +68,6 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 @login_required
 def logout(request):
+    messages.success(request, f"{request.user.username} Вы вышли из аккаунта")
     auth.logout(request)
     return redirect(reverse('main:index'))
